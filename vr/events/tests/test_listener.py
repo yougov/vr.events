6c0aa7664a377dfe1bf6535bee49d20a9c0df0eb
Jason R. Coombs <jaraco@jaraco.com>
2015-12-07 10:18:01 -0500

    Rely on pytest rewrite asserts for clearer tests and nicer results on failure.

--- vr/events/tests/test_listener.py
+++ vr/events/tests/test_listener.py
@@ -1,5 +1,4 @@
 """Unit tests for :class:`vr.events.Listener`."""
-import unittest
 from unittest.mock import MagicMock, patch
 
 from vr.events import Listener
@@ -9,11 +8,11 @@ class MockRedis(MagicMock):
     pass
 
 
-class ListenerTestCase(unittest.TestCase):
+class ListenerTestCase:
 
     def test_create_listener(self):
         with patch('redis.StrictRedis', MockRedis):
             listener = Listener('redis://localhost:6379/0', channels=['foo'])
 
-        self.assertIsInstance(listener.rcon, MockRedis)
-        self.assertEqual(listener.channels, ['foo'])
+        assert isinstance(listener.rcon, MockRedis)
+        assert listener.channels == ['foo']
